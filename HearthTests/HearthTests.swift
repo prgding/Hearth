e588da81f2c190bb2290c2727e7751ec12703be0
dings <1203823603@qq.com>
2026-05-25 12:51:18 +0800

    Initial Commit

---
+++ HearthTests/HearthTests.swift
@@ -0,0 +1,19 @@
+//
+//  HearthTests.swift
+//  HearthTests
+//
+//  Created by dingshuai on 5/25/26.
+//
+
+import Testing
+@testable import Hearth
+
+struct HearthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
